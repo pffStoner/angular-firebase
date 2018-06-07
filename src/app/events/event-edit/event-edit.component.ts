41febbdae97fc29e9f3105961ef1317c76fe16d0
Dimitar Kokotanekov <acid901@mail.bg>
2018-06-07 18:08:14 +0300

    started adding new tasks to task list

--- src/app/events/event-edit/event-edit.component.ts
+++ src/app/events/event-edit/event-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Form, FormGroup, FormControl } from '@angular/forms';
+import { Form, FormGroup, FormControl, FormArray } from '@angular/forms';
 import { EventService } from '../../services/event.service';
 
 @Component({
@@ -27,23 +27,46 @@ export class EventEditComponent implements OnInit {
         }
       );
   }
+  onAddingTask() {
+  (<FormArray> this.eventForm.get('tasks')).push(
+    new FormGroup({
+      'name': new FormControl(),
+      'description': new FormControl(),
+    })
+  )
+  }
 
   private initForm() {
   let eventName = '';
   let eventImgPath = '';
   let description = '';
+  // tslint:disable-next-line:prefer-const
+  let eventTasks = new FormArray([]);
 
   if (this.editMode) {
     const event = this.eService.getEvent(this.id);
     eventName = event.name;
     eventImgPath = event.img;
     description = event.description;
+    // proverqvame dali imame nqkakva zadachi
+    if (event['task']) {
+        // tslint:disable-next-line:prefer-const
+        for (let task of event.task) {
+            eventTasks.push(
+              new FormGroup({
+                'name': new FormControl(task.name),
+                'description': new FormControl(task.description),
+              })
+            );
+        }
+    }
   }
   this.eventForm = new FormGroup({
     // trqbva da suvpadat s formControlName v .html
     'name': new FormControl(eventName),
     'imgUrl': new FormControl(eventImgPath),
-    'description': new FormControl(description)
+    'description': new FormControl(description),
+    'tasks': eventTasks
   });
   }
   onSubmit() {
